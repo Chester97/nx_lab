e0eb73cc5b0931fb9711473b4e26ead1c92857d1
Kamil Kowalczuk-Fijałkowski <kamil.kowalczuk.fijakowski@itv.com>
2022-10-26 09:55:51 +0200

    foo

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,26 @@
+const path = require('path');
+
 module.exports = {
   stories: [],
   addons: ['@storybook/addon-essentials'],
   // uncomment the property below if you want to apply some webpack config globally
-  // webpackFinal: async (config, { configType }) => {
-  //   // Make whatever fine-grained changes you need that should apply to all storybook configs
+  webpackFinal: async (config, { configType }) => {
+    // Remove the existing css rule
+    config.module.rules = config.module.rules.filter(
+      f => f.test.toString() !== '/\\.css$/'
+    );
 
-  //   // Return the altered config
-  //   return config;
-  // },
+    config.module.rules.push({
+      test: /\.css$/,
+      use: ['style-loader', {
+        loader: 'css-loader',
+        options: {
+          modules: true, // Enable modules to help you using className
+        }
+      }],
+      include: path.resolve(__dirname, '../src'),
+    });
+
+    return config;
+  },
 };
