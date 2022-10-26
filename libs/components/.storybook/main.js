958819bf11af118710ac66f82d5382c15ab9a4ce
Kamil Kowalczuk-Fijałkowski <kamil.kowalczuk.fijakowski@itv.com>
2022-10-26 12:49:27 +0200

    bar

--- libs/components/.storybook/main.js
+++ libs/components/.storybook/main.js
@@ -30,3 +30,37 @@ module.exports = {
     return config;
   },
 };
+
+
+// const path = require('path');
+//
+// const rootMain = require('../../../.storybook/main');
+//
+// module.exports = {
+//   ...rootMain,
+//
+//   core: { ...rootMain.core, builder: 'webpack5' },
+//
+//   stories: [
+//     ...rootMain.stories,
+//     '../src/lib/**/*.stories.mdx',
+//     '../src/lib/**/*.stories.@(js|jsx|ts|tsx)',
+//   ],
+//   addons: [...rootMain.addons, '@nrwl/react/plugins/storybook'],
+//   webpackFinal: async (config, { configType }) => {
+//     // apply any global webpack configs that might have been specified in .storybook/main.js
+//     if (rootMain.webpackFinal) {
+//       config = await rootMain.webpackFinal(config, { configType });
+//     }
+//
+//     // add your own webpack tweaks if needed
+//
+//     config.module.rules.push({
+//       test: /\.scss$/,
+//       use: ['style-loader', 'css-loader?modules&importLoaders', 'sass-loader'],
+//       include: path.resolve(__dirname, '../'),
+//     });
+//
+//     return config;
+//   },
+// };
