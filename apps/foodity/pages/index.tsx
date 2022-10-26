3afecc1111e567ae049f0a319b2b0329a2c9eae8
Kamil Kowalczuk-Fijałkowski <kamil.kowalczuk.fijakowski@itv.com>
2022-10-26 19:59:22 +0200

    v03

--- apps/foodity/pages/index.tsx
+++ apps/foodity/pages/index.tsx
@@ -19,6 +19,7 @@ export function Index() {
           <div id="welcome">
             <h1>
               <span> Hello there, </span>
+              <span> Hello there NIKE, </span>
               Welcome foodidasdsadasdsadty-app 👋
             </h1>
             <Components />
