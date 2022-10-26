aeaf061f4e4971ffa4b4397bbac5f4e4b3c95301
Kamil Kowalczuk-Fijałkowski <kamil.kowalczuk.fijakowski@itv.com>
2022-10-26 11:22:15 +0200

    test

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,23 +4,23 @@ module.exports = {
   stories: [],
   addons: ['@storybook/addon-essentials'],
   // uncomment the property below if you want to apply some webpack config globally
-  webpackFinal: async (config, { configType }) => {
-    // Remove the existing css rule
-    config.module.rules = config.module.rules.filter(
-      f => f.test.toString() !== '/\\.css$/'
-    );
-
-    config.module.rules.push({
-      test: /\.css$/,
-      use: ['style-loader', {
-        loader: 'css-loader',
-        options: {
-          modules: true, // Enable modules to help you using className
-        }
-      }],
-      include: path.resolve(__dirname, '../src'),
-    });
-
-    return config;
-  },
+  // webpackFinal: async (config, { configType }) => {
+  //   // Remove the existing css rule
+  //   config.module.rules = config.module.rules.filter(
+  //     f => f.test.toString() !== '/\\.css$/'
+  //   );
+  //
+  //   config.module.rules.push({
+  //     test: /\.css$/,
+  //     use: ['style-loader', {
+  //       loader: 'css-loader',
+  //       options: {
+  //         modules: true, // Enable modules to help you using className
+  //       }
+  //     }],
+  //     include: path.resolve(__dirname, '../src'),
+  //   });
+  //
+  //   return config;
+  // },
 };
