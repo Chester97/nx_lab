86eaa9af397b70dee232144d2d47fcfb33778c9d
Kamil Kowalczuk-Fijałkowski <kamil.kowalczuk.fijakowski@itv.com>
2022-10-27 19:11:50 +0200

    v11

--- apps/foodity/pages/_app.tsx
+++ apps/foodity/pages/_app.tsx
@@ -14,8 +14,6 @@ function CustomApp({ Component, pageProps }: AppProps) {
         <Components />
         <h1>siema</h1>
         <h1>siema</h1>
-        <h1>siema</h1>
-        <h1>siema</h1>
       </main>
     </>
   );
