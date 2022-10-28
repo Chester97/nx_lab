d84b117bcaa9b9401a5a7e2fe8737fe5156ed305
Kamil Kowalczuk-Fijałkowski <kamil.kowalczuk.fijakowski@itv.com>
2022-10-28 18:07:41 +0200

    both

--- apps/foodity/pages/_document.tsx
+++ apps/foodity/pages/_document.tsx
@@ -37,6 +37,7 @@ export default class CustomDocument extends Document {
           <Main />
           <NextScript />
           hgipfsgjlifd
+          hgipfsgjlifd
         </body>
       </Html>
     );
