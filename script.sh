fb7bea046fe4b67492d8463739f459b077c4fe8a
Arbi <arbikow1@gmail.com>
2022-10-30 13:05:21 +0100

    xd

--- script.sh
+++ script.sh
@@ -4,7 +4,7 @@ echo "VERCEL_ENV: $VERCEL_ENV"
 echo "VERCEL_ENV_PRE: $VERCEL_ENV_PRE"
 echo "VERCEL_STAGING: $VERCEL_VERCEL_STAGING"
 
-if [[ "$VERCEL_STAGING" == "vercel_staging" ]] ; then
+if [[ "$VERCEL_ENV" == "production" || "$VERCEL_ENV_PRE" == "preview" || "$VERCEL_STAGING" == "vercel_staging" ]] ; then
   # Proceed with the build
   echo "✅ - Build can proceed"
   exit 1;
